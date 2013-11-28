0c8966e7de39c9c03af7b6140b2f03ac1b7531d0
twinther <tommy@winther.nu>
2013-11-28 20:49:08 +0100

    update for Frodo/Gotham

--- addon.py
+++ addon.py
@@ -1,3 +1,22 @@
+#
+#      Copyright (C) 2013 Tommy Winther
+#      http://tommy.winther.nu
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with XBMC; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+#
 import os
 import sys
 import urlparse
@@ -7,69 +26,74 @@ import xbmcgui
 import xbmcplugin
 import xbmcaddon
 
+import buggalo
+
 BASE_URL = 'http://gaffa.dk/tv/archive/'
 VIDEO_URL = 'http://videobackup.gaffa.dk/videos/%s.mov'
 
-class GaffaTVAddon(object):
-    def showOverview(self):
-        u = urllib2.urlopen(BASE_URL)
-        html = u.read()
-        u.close()
 
-        for m in re.finditer("<option[ ]+value='([0-9]+)'>([^<]+)</option>", html):
-            category_id = m.group(1)
-            title = m.group(2)
+def showOverview():
+    u = urllib2.urlopen(BASE_URL)
+    html = u.read()
+    u.close()
 
-            item = xbmcgui.ListItem(title, iconImage = ICON)
-            item.setProperty('Fanart_Image', FANART)
-            item.setInfo(type='video', infoLabels={
-                'title': title
-                })
+    for m in re.finditer("<option[ ]+value='([0-9]+)'>([^<]+)</option>", html):
+        category_id = m.group(1)
+        title = m.group(2)
 
-            url = PATH + '?id=' + category_id + '&page=1'
-            xbmcplugin.addDirectoryItem(HANDLE, url, item, isFolder = True)
+        item = xbmcgui.ListItem(title, iconImage=ICON)
+        item.setProperty('Fanart_Image', FANART)
+        item.setInfo(type='video', infoLabels={
+            'title': title
+        })
 
-        xbmcplugin.endOfDirectory(HANDLE)
+        url = PATH + '?id=' + category_id + '&page=1'
+        xbmcplugin.addDirectoryItem(HANDLE, url, item, isFolder=True)
 
-    def showCategory(self, category_id, page = 1):
-        url = BASE_URL + str(category_id)
-        if page > 1:
-            url += '/page:%s' % page
-        print url
+    xbmcplugin.endOfDirectory(HANDLE)
 
-        u = urllib2.urlopen(url)
-        html = u.read()
-        u.close()
 
-        for m in re.finditer('<a href="/tv/clip/([0-9]+)" title="(.*?)"><img src=\'([^\']+)\'.*?<p>(.*?)</p>', html, re.DOTALL):
-            title = m.group(2)[9:]
-            icon = m.group(3).replace('_193.jpg', '_640.jpg')
-            description = m.group(4)
+def showCategory(category_id, page=1):
+    url = BASE_URL + str(category_id)
+    if page > 1:
+        url += '/page:%s' % page
+    print url
 
-            m = re.search('splash/(.*?)_193', m.group(3))
-            if m:
-                url = VIDEO_URL % m.group(1)
+    u = urllib2.urlopen(url)
+    html = u.read()
+    u.close()
 
-            item = xbmcgui.ListItem(title, iconImage = icon, thumbnailImage = icon)
-            item.setProperty('Fanart_Image', icon)
-            item.setInfo(type='video', infoLabels={
-                'title': title,
-                'plot' : description,
-                'studio' : ADDON.getAddonInfo('name')
-            })
+    for m in re.finditer('<a href="/tv/clip/([0-9]+)" title="(.*?)"><img src=\'([^\']+)\'.*?<p>(.*?)</p>', html,
+                         re.DOTALL):
+        title = m.group(2)[9:]
+        icon = m.group(3).replace('_193.jpg', '_640.jpg')
+        description = m.group(4)
 
-            xbmcplugin.addDirectoryItem(HANDLE, url, item)
+        m = re.search('splash/(.*?)_193', m.group(3))
+        if m:
+            url = VIDEO_URL % m.group(1)
 
-        if re.search('class="next"', html) is not None:
-            item = xbmcgui.ListItem(ADDON.getLocalizedString(30000), iconImage = ICON)
-            item.setProperty('Fanart_Image', FANART)
-            url = PATH + '?id=' + category_id + '&page=' + str(page + 1)
-            xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
+        item = xbmcgui.ListItem(title, iconImage=icon, thumbnailImage=icon)
+        item.setProperty('Fanart_Image', icon)
+        item.setInfo(type='video', infoLabels={
+            'title': title,
+            'plot': description,
+            'studio': ADDON.getAddonInfo('name')
+        })
 
-        xbmcplugin.endOfDirectory(HANDLE)
+        xbmcplugin.addDirectoryItem(HANDLE, url, item)
+
+    if re.search('class="next"', html) is not None:
+        item = xbmcgui.ListItem(ADDON.getLocalizedString(30000), iconImage=ICON)
+        item.setProperty('Fanart_Image', FANART)
+        url = PATH + '?id=' + category_id + '&page=' + str(page + 1)
+        xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
+
+    xbmcplugin.endOfDirectory(HANDLE)
+
 
 if __name__ == '__main__':
-    ADDON = xbmcaddon.Addon(id = 'plugin.video.gaffa.tv')
+    ADDON = xbmcaddon.Addon()
     PATH = sys.argv[0]
     HANDLE = int(sys.argv[1])
     PARAMS = urlparse.parse_qs(sys.argv[2][1:])
@@ -77,10 +101,13 @@ if __name__ == '__main__':
     ICON = os.path.join(ADDON.getAddonInfo('path'), 'icon.png')
     FANART = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
 
-    gtv = GaffaTVAddon()
-    if PARAMS.has_key('id') and PARAMS.has_key('page'):
-        gtv.showCategory(PARAMS['id'][0], int(PARAMS['page'][0]))
-    elif PARAMS.has_key('id'):
-        gtv.showCategory(PARAMS['id'])
-    else:
-        gtv.showOverview()
+    buggalo.SUBMIT_URL = 'http://tommy.winther.nu/exception/submit.php'
+    try:
+        if 'id' in PARAMS and 'page' in PARAMS:
+            showCategory(PARAMS['id'][0], int(PARAMS['page'][0]))
+        elif 'id' in PARAMS:
+            showCategory(PARAMS['id'])
+        else:
+            showOverview()
+    except:
+        buggalo.onExceptionRaised()
